f0fdb384a2441f060bccf60e48dc7fcc483b3521
Cauan EngDev <reisu.d.cauan@proton.me>
2026-08-09 17:01:45 -0300

    Setup e Primeiros Passos

---
+++ src/test/java/com/cauandev/proposalmanagement/ProposalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cauandev.proposalmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProposalManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
