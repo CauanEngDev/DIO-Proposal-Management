bbf7b441b6271de1a27721a3b23d1b1a450f5ddf
Cauan EngDev <reisu.d.cauan@proton.me>
2026-08-12 08:12:37 -0300

    Persistindo Entitades

---
+++ src/main/java/com/cauandev/proposalmanagement/proposal/infrastructure/persistence/repository/ProposalEntityRepository.java
@@ -0,0 +1,11 @@
+package com.cauandev.proposalmanagement.proposal.infrastructure.persistence.repository;
+
+import com.cauandev.proposalmanagement.proposal.infrastructure.persistence.entity.ProposalEntity;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ProposalEntityRepository extends CrudRepository<ProposalEntity, UUID> {
+    List<ProposalEntity> findAllByOwnerId(UUID ownerId);
+}
